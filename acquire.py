8d0b7611a98af1b21c0ce370df57d5f851af0986
ArnolValadez <a7xfav@gmail.com>
2021-08-30 18:05:43 -0500

    add acquire

---
+++ acquire.py
@@ -0,0 +1,22 @@
+from env import host, password, user, get_connection
+import pandas as pd
+import numpy as np
+import os
+
+def get_telco_data():
+
+    my_query = ''' SELECT c.*, pt.payment_type, ist.internet_service_type, ct.contract_type
+    FROM customers as c
+    JOIN payment_types as pt on c.payment_type_id = pt.payment_type_id
+    JOIN internet_service_types AS ist on ist.internet_service_type_id = c.internet_service_type_id
+    JOIN contract_types as ct ON ct.contract_type_id = c.contract_type_id;'''
+
+    filename = "telco_data.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        telco_df = pd.read_sql(my_telco_query, get_db_url('telco_churn'))
+        telco_df.to_csv(filename, index = False)
+        
+    return telco_df
